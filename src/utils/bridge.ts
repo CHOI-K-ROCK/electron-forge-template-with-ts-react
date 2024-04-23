73d9a53285214d828b5d6e0cf53143ec1c2eae70
K_ROCK_ <chl4842@gmail.com>
2024-04-23 15:43:32 +0900

    Update bridge.ts

--- src/utils/bridge.ts
+++ src/utils/bridge.ts
@@ -4,6 +4,6 @@ import { CustomWindow } from "../@types/window";
 
 declare let window: CustomWindow; // window 객체 타입 오류 수정
 
-export const invoke = (key: InvokeChannels) => {
-    window.bridge.invoke(key);
+export const invoke = async (key: InvokeChannels) => {
+    return await window.bridge.invoke(key);
 };
